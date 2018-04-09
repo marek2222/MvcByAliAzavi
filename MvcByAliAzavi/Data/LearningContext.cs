9d9d70f3e09461ba6709e30f5ec26723cb5ef3fe
marek2222 <marekmusial2@o2.pl>
2018-04-09 12:38:17 +0200

    AliAdravi: 3.2:  LearningContext

--- MvcByAliAzavi/Data/LearningContext.cs
+++ MvcByAliAzavi/Data/LearningContext.cs
@@ -1,11 +1,14 @@
-﻿using System;
+﻿using MvcByAliAzavi.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
 namespace MvcByAliAzavi.Data
 {
-  public class LearningContext
+  public class LearningContext : DbContext
   {
+    public DbSet<Category> Categories { get; set; }
   }
 }
