cb3956f7a20aa483bd3f92ad2e27514ebc4fa8b3
marek2222 <marekmusial2@o2.pl>
2018-04-09 12:31:55 +0200

    AliAdravi: 2.1:   Dodanie modelu: Category, kontrolera z listą: CategoryController i widoku na podstawie Index

---
+++ MvcByAliAzavi/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using MvcByAliAzavi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcByAliAzavi.Controllers
+{
+  public class CategoryController : Controller
+  {
+    // GET: Category
+    public ActionResult Index()
+    {
+      var categories = new List<Category>();
+      categories.Add(new Category { CategoryId = 1, CategoryName = "Laptop" });
+      categories.Add(new Category { CategoryId = 1, CategoryName = "Software" });
+      categories.Add(new Category { CategoryId = 1, CategoryName = "Accessories" });
+
+      return View(categories);
+    }
+
+  }
+}
---
+++ MvcByAliAzavi/Models/Category.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcByAliAzavi.Models
+{
+  public class Category
+  {
+    [Key]
+    public int CategoryId { get; set; }
+
+    [Required]
+    [Display(Name = "Category Name")]
+    [StringLength(30, ErrorMessage = "The {0} must be between {2} to {1} characters", MinimumLength = 5)]
+    public String CategoryName { get; set; }
+
+    [Display(Name = "Is Active")]
+    public bool IsActive { get; set; }
+  }
+}
