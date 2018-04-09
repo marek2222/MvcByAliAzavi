aaf9d27c10ed0f235cb100d77cf084cb16755271
marek2222 <marekmusial2@o2.pl>
2018-04-09 11:18:06 +0200

    AliAdravi: 1.1: Utworzenie projektu

---
+++ MvcByAliAzavi/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcByAliAzavi.Startup))]
+namespace MvcByAliAzavi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
